6a1f9bf4f0e5a1f02d55486b1c0d0426a2b00954
carlosismaelad <carlosismaelad@outlook.com>
2023-11-30 23:20:40 -0300

    Trat exceçoes, primeira solução - muito ruim

--- dataTratDeExcecoes/src/App.java
+++ dataTratDeExcecoes/src/App.java
@@ -1,5 +1,50 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entities.Reservation;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws ParseException {
+
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int roomNumber = sc.nextInt();
+        System.out.print("Check-in date: ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out date: ");
+        Date checkOut = sdf.parse(sc.next());
+
+        if (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date.");
+        } else {
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter date to update the reservation: ");
+            System.out.print("Check-in date: ");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date: ");
+            checkOut = sdf.parse(sc.next());
+
+            Date now = new Date();
+            if (checkIn.before(now) || checkOut.before(now)) {
+                System.out.println("Error in reservation: Reservation dates for update must be future dates.");
+            } else if (!checkOut.after(checkIn)) {
+                System.out.println("Error in reservation: Check-out date must be after check-in date.");
+            } else {
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println("Updated reservation: " + reservation);
+            }
+
+        }
+
+        sc.close();
+
     }
+
 }
