f215cb4232a406c8f64f065a639ad627b857856f
carlosismaelad <carlosismaelad@outlook.com>
2023-12-04 21:38:26 -0300

    Exer final sobre tratamento de exceções - Contas bancárias

---
+++ exerFinalTratamentoDeExcecoesContasBancarias/src/App.java
@@ -0,0 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+import exception.BusinessException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe os dados da conta");
+        System.out.print("Numero: ");
+        int number = sc.nextInt();
+        System.out.print("Titular: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Saldo inicial: ");
+        double balance = sc.nextDouble();
+        System.out.print("Limite de saque: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Informe uma quantia para sacar: ");
+        double amount = sc.nextDouble();
+
+        try {
+            acc.withdraw(amount);
+            System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+        } catch (BusinessException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+}
---
+++ exerFinalTratamentoDeExcecoesContasBancarias/src/exception/BusinessException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String msg) {
+        super(msg);
+    }
+
+}
